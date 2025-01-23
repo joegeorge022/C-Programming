5689c81498f819b2d59465fa92f56c62e738583c
Joe <127773439+joegeorge022@users.noreply.github.com>
2025-01-23 20:04:41 +0530

    Create 1.4.c

---
+++ SJCET/practice_questions/1.4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    /* If cost price and selling price of an item are input through the keyboard, 
+    write a program to determine whether the seller has made profit or incurred loss. 
+    Also determine how much profit he made or loss he incurred.*/
+
+    int profit,loss, costPrice,sellPrice;
+
+    printf("Enter the cost of the item: ");
+    scanf("%d", &costPrice);
+    
+    printf("Enter the price at which the item was sold: ");
+    scanf("%d", &sellPrice);
+    
+    if (costPrice > sellPrice) {
+        loss = costPrice - sellPrice;
+        printf("You made a loss💀. \nLoss: %d\n", loss);
+    } 
+    else if (costPrice == sellPrice) {
+        printf("Neither loss nor profit was made🫠.\n");
+    } 
+    else {
+        profit = sellPrice - costPrice;
+        printf("You made a profit✨. \nProfit: %d\n", profit);
+    }
+
+    return 0;
+}
+
+
+    // idk if i should use snake_case or camelCase for variables. I dont think they really matter.
+    // Depends on the language. C uses camelCase so I guess thats better
