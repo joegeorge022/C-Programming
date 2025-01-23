e9f4ad303840a3330d6b7fef0857ca36535e5324
Joe <127773439+joegeorge022@users.noreply.github.com>
2025-01-23 20:05:25 +0530

    Update 1.3.c

--- SJCET/practice_questions/1.3.c
+++ SJCET/practice_questions/1.3.c
@@ -2,6 +2,10 @@
 #include <math.h>
 
 int main(){
+    /* 1.3 Write a C program to read the age of a candidate and determine whether it is eligible for casting his/her own vote.
+    Test Data: 21
+    Expected Output: Congratulation! You are eligible for casting your vote.*/
+    
     int age;
     printf("Enter your age: ");
     scanf("%d", &age);
