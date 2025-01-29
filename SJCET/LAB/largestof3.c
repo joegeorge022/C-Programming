8d6067b941452f619c4040b3fb6ae36c637f556b
Joe <127773439+joegeorge022@users.noreply.github.com>
2025-01-29 14:47:26 +0530

    Create largestof3.c

---
+++ SJCET/LAB/largestof3.c
@@ -0,0 +1,34 @@
+
+int main(){
+    int num1,num2,num3;
+    
+    printf("Enter numbers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+    
+    if (num1>num2){
+        if(num1>num3){
+            printf("%d is largest.\n", num1);
+        } else if (num1 = num3){
+            printf("%d and %d are same \n", num1, num3);
+        } else {
+            printf("%d is largest.\n", num3);
+        }
+    } else if (num2 > num1){
+        if(num2 > num3){
+            printf("%d is largest.\n",num2);
+        } else if (num2 = num3){
+            printf("%d and %d are same \n", num2, num3);
+        } else {
+            printf("%d is largest.\n", num3);
+        }
+    } else{
+        if(num2=num3){
+            printf("Three numbers are same. \n");
+        }
+        else{
+            printf("%d and %d are same \n", num1, num2);
+        }
+    }
+    
+    return 0;
+}
