9f27d347c704ee477e6379fb220b6e104da37437
Joe <127773439+joegeorge022@users.noreply.github.com>
2025-01-29 14:46:19 +0530

    Create calculator.c

---
+++ SJCET/LAB/calculator.c
@@ -0,0 +1,44 @@
+int main(){
+    char operator;
+    int num1, num2, result;
+    
+    printf("Choose an operator +, -, *, /, %%: ");
+    scanf("%c", &operator);
+    
+    printf("Enter number1: ");
+    scanf("%d", &num1);
+
+    printf("Enter number2: ");
+    scanf("%d", &num2);
+    
+    switch(operator){
+        case '+':
+        result=num1+num2;
+        printf("Sum of the 2 numbers is %d \n", result);
+        break;
+
+        case '-':
+        result=num1-num2;
+        printf("Difference of the 2 numbers is %d \n", result);
+        break;
+
+        case '*':
+        result=num1*num2;
+        printf("Product of the 2 numbers is %d \n", result);
+        break;
+
+        case '/':
+        result=num1/num2;
+        printf("Quotient of the 2 numbers is %d \n",result);
+        break;
+
+        case '%':
+        result=num1%num2;
+        printf("Remainder2 numbers is %d \n",result);
+        break;
+
+        default:
+        printf("Invalid Input! \n");
+    }
+    return 0;
+}
