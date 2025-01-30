2b3de4a776b69765bea9ad4f2cb6b4d5347be5f0
Joe <127773439+joegeorge022@users.noreply.github.com>
2025-01-30 23:17:38 +0530

    Update 3.factorial.c

--- SJCET/LAB/3.factorial.c
+++ SJCET/LAB/3.factorial.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 
-int main(){
+int main() {
     /*3. Factorial Using While Loop: Write a program to calculate the factorial of a
     given number using a while loop. Validate the input to ensure it is a positive
     integer.*/
     
-    int num, result=1;
-    
+    int num, result = 1, i;
+
     printf("Enter Number: ");
     scanf("%d", &num);
-    
-    if(num>0){
-        while(num>0){
-            result *= num;
-            num -= 1;    
+
+    if (num < 0) {
+        printf("Enter a non-negative number.\n");
+    } else {
+        i = num;
+        while (i > 0) {
+            result *= i;
+            i--;
         }
-        printf("%d is the factorial. \n",result);
-    } else{
-        printf("Enter a positive number. \n");
+        printf("Factorial of %d is %d\n", num, result);
     }
-    
+
     return 0;
 }
