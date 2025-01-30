f8331b923c3e6ec10257cda38ca2198f40f194b1
Joe <127773439+joegeorge022@users.noreply.github.com>
2025-01-30 22:20:59 +0530

    Update 1.largestof3.c

--- SJCET/LAB/1.largestof3.c
+++ SJCET/LAB/1.largestof3.c
@@ -37,3 +37,34 @@ int main(){
     
     return 0;
 }
+
+
+
+/*
+#include <stdio.h>
+
+int main() {
+    int num1, num2, num3;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+
+    if (num1 == num2 && num2 == num3) {
+        printf("Three numbers are the same.\n");
+    } else {
+        if (num1 >= num2 && num1 >= num3) {
+            printf("%d is the largest.\n", num1);
+        } else if (num2 >= num1 && num2 >= num3) {
+            printf("%d is the largest.\n", num2);
+        } else {
+            printf("%d is the largest.\n", num3);
+        }
+
+        if (num1 == num2 || num1 == num3 || num2 == num3) {
+            printf("Two numbers are the same.\n");
+        }
+    }
+
+    return 0;
+}
+*/
