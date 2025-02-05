ac13cace8b326730247113bd82048028d9e913dc
Joe <127773439+joegeorge022@users.noreply.github.com>
2025-02-05 18:10:08 +0530

    Update 5.min_max.c

--- SJCET/LAB/5.min_max.c
+++ SJCET/LAB/5.min_max.c
@@ -1,30 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    int size, min = 1, max = 1;
-    printf("Enter the size of the array: ");
+    int size, min = 0, max = 0;
+    
+    printf("Enter size of array: ");
     scanf("%d", &size);
-    
+
     int array[size];
-
-    for (int a = 0; a < size; a++) {
-        printf("Enter the %dth element: ", a+1);
-        scanf("%d", &array[a]);
-    }
+    printf("Enter elements: ");
     
-    int largest = array[0], smallest = array[0];
-    
-    for (int i = 1; i < size; i++) {
-        if (array[i] > largest) {
-            largest = array[i];
-            max = i + 1;
-        }
-        if (array[i] < smallest) {
-            smallest = array[i];
-            min = i + 1;
-        }
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &array[i]);
+        if (array[i] > array[max]) max = i;
+        if (array[i] < array[min]) min = i;
     }
 
-    printf("\n%d is the smallest element and its index is %d.\n", smallest, min);
-    printf("%d is the largest element and its index is %d.\n", largest, max);
+    printf("\nSmallest: %d at index %d\n", array[min], min + 1);
+    printf("Largest: %d at index %d\n", array[max], max + 1);
+
+    return 0;
 }
