db5e9045d0a04cd0414b6365d00daa587b7049b7
Joe <127773439+joegeorge022@users.noreply.github.com>
2025-02-05 14:43:40 +0530

    Update 5.min_max.c

--- SJCET/LAB/5.min_max.c
+++ SJCET/LAB/5.min_max.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
 
 int main() {
-
+    
     //asks for the size of the array
     int size;
     printf("Enter the size of the array: ");
     scanf("%d", &size);
     
-    //inputs the elements accordingly
     int array[size];
+
+    //inputs the elements accordingly    
     for (int a = 0; a < size; a++) {
         printf("Enter the %dth element: ", a+1);
         scanf("%d", &array[a]);
     }
     
-    //bubble sort algorithm
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                int temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-            }
+    int largest = array[0], smallest = array[0];
+    
+    //finds largest element
+    for (int i = 1; i < size; i++) {
+        if (array[i] > largest) {
+            largest = array[i];
         }
     }
-    
-    //prints the largest and smallest numbers.
-    printf("\n%d is the smallest element.\n",array[0]);
-    printf("%d is the largest element.\n",array[size-1]);
+
+    //finds smallest element
+    for (int i = 1; i < size; i++) {
+        if (array[i] < smallest) {
+            smallest = array[i];
+        }
+    }
+
+    printf("\n%d is the smallest element.\n", smallest);
+    printf("%d is the largest element.\n", largest);
 }
