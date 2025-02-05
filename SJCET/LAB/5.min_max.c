2289fc4ab08dc56fa4390c5f2061dae922499cdd
Joe <127773439+joegeorge022@users.noreply.github.com>
2025-02-05 18:11:17 +0530

    Update 5.min_max.c

--- SJCET/LAB/5.min_max.c
+++ SJCET/LAB/5.min_max.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
 
 int main() {
+    int size, min = 1, max = 1;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    
+    int array[size];
+
+    for (int a = 0; a < size; a++) {
+        printf("Enter the %dth element: ", a+1);
+        scanf("%d", &array[a]);
+    }
+    
+    int largest = array[0], smallest = array[0];
+    
+    for (int i = 1; i < size; i++) {
+        if (array[i] > largest) {
+            largest = array[i];
+            max = i + 1;
+        }
+        if (array[i] < smallest) {
+            smallest = array[i];
+            min = i + 1;
+        }
+    }
+
+    printf("\n%d is the smallest element and its index is %d.\n", smallest, min);
+    printf("%d is the largest element and its index is %d.\n", largest, max);
+}
+
+
+
+
+/*
+#include <stdio.h>
+
+int main() {
     int size, min = 0, max = 0;
     
     printf("Enter size of array: ");
@@ -20,3 +55,4 @@ int main() {
 
     return 0;
 }
+*/
