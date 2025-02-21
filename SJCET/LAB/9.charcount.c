a97b79409368e997afa77b3c1df75d5a0c4e8dcb
Joe <127773439+joegeorge022@users.noreply.github.com>
2025-02-21 19:00:57 +0530

    Update 9.charcount.c

--- SJCET/LAB/9.charcount.c
+++ SJCET/LAB/9.charcount.c
@@ -9,17 +9,27 @@ int main(){
     /* 9.Character Frequency Count: Write a program to count the
     frequency of each character in a given string. Ignore case
     sensitivity and consider only alphabetical characters.*/
-    
-    char str[15];
-    int count = 0;
-    
-    printf("Enter the string: ");
-    gets(str);
-    
-    for(int i=0; str[i]!='\0'; i++){
-        count++;
+    char str[1000];
+    int freq[26] = {0};
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        char ch = str[i];
+
+        if (ch >= 'A' && ch <= 'Z') {
+            ch = ch + 32;
+        }
+
+        if (ch >= 'a' && ch <= 'z') {
+            freq[ch - 97]++;        }
     }
-    
-    printf("Letter count: %d\n",count);
-    
+
+    printf("Character Frequency:\n");
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] > 0) {
+            printf("%c: %d\n", i + 97, freq[i]);
+        }
+    }
 }
