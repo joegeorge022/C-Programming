bf6c7f88d9461204a87987d9bffa9899d6f54700
Joe <127773439+joegeorge022@users.noreply.github.com>
2025-03-26 14:05:03 +0530

    Update 10.power.c

--- SJCET/LAB/10.power.c
+++ SJCET/LAB/10.power.c
@@ -7,8 +7,8 @@
 #include <stdio.h>
 
 int power(int base, int pow){
-    if (pow==1){
-        return base;
+    if (pow==0){
+        return 1;
     } else {
         return base * power(base, pow-1);
     }
